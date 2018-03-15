99c51f00a0612aba3fcc4af61a5590e211847480
Michael Adelson <mike.adelson314@gmail.com>
2018-03-15 07:10:08 -0400

    initial commit

---
+++ Bitwise.Tests/BitsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Bitwise;
+
+namespace Bitwise.Tests
+{
+    [TestFixture]
+    public class BitsTest
+    {
+        [Test]
+        public void TestHasAnyFlagInt64()
+        {
+            Assert.AreEqual(true, ((long)1).HasAnyFlag(1));
+        }
+    }
+}
---
+++ Bitwise/Bits.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bitwise
+{
+    public static class Bits
+    {
+        private const int SizeOfInt64InBits = 64;
+
+        public static bool HasAnyFlag(this long value, long flags) => (value & flags) != 0;
+    }
+}
